f70fe984c1d5bb5f04b619444b947a3c459dff79
Ken Chatfield <ken.chatfield@gmail.com>
2013-12-12 15:28:12 +0000

    Added simple bootstrap styled landing page

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ Simple Django Landing Page
 ==========================
 
 Example of simple landing page with user email address registration in Django
+
+    $ python manage.py syncdb
+	$ cd userreg
+	$ bower install
+	$ cd ../
+	$ python manage.py runserver
--- userreg/forms.py
+++ userreg/forms.py
@@ -2,6 +2,18 @@ from django import forms
 from userreg.models import RegisteredUser
 
 class RegisterUserForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+        for f_name in self.fields:
+            classes = self.fields[f_name].widget.attrs.get('class', '')
+            if not classes:
+                classes += ' '
+            classes += 'form-control'
+            self.fields[f_name].widget.attrs['class'] = classes
+                    
     class Meta:
         model = RegisteredUser
         exclude = ('date','ip')
+        widgets = {
+            'email': forms.TextInput(attrs={'placeholder': 'Your email address'})
+        }
